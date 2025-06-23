5d01edfc7ae7dc231a995bb02913cf4cc537d289
alexandre-mateos <mateosalexandre95@gmail.com>
2025-06-23 14:57:23 +0200

    correction erreur de redirection vers la page d'accueil, ajout d'une boucle et modification de l'url de recherche pour afficher plusieurs pays

--- tp-php/form-country.php
+++ tp-php/form-country.php
@@ -1,5 +1,5 @@
 <body>
-<form action="form-treatment.php" method="post">
+<form action="/tp-php/form-treatment.php" method="post">
       <label for="country">Country</label>
       <input id="country" name="country">
       <button type="submit">Send</button>
--- tp-php/form-treatment.php
+++ tp-php/form-treatment.php
@@ -1,14 +1,16 @@
 <?php
 
+
 //vérifie d'abord si l'utilisateur à bien rempli le champs. Si ce n'est pas le cas retour accueil
 //avec code error2 passé dans l'URL
 
 if(isset($_POST["country"]) && strlen($_POST["country"]) >= 1) {
 
     //Va taper sur l'API via l'URL reconstruite avec l'entrée utilisateur
-    //ON supprime les espaces en début et fin de string avec trim()
+    //On supprime les espaces en début et fin de string avec trim(), et on met en minuscule.
       $country = trim(htmlspecialchars($_POST["country"]));
-      $countryRequest = "https://restcountries.com/v3.1/name/" . $country . "?fullText=true";
+      $countryLowerCase = strtolower($country);
+      $countryRequest = "https://restcountries.com/v3.1/name/" . $countryLowerCase;
       $dataCountry = @file_get_contents($countryRequest);
 
       //L'ajout de @ devant file_get_content revois un booléen. Vrai si la requète a fonctionner
@@ -17,6 +19,7 @@ if(isset($_POST["country"]) && strlen($_POST["country"]) >= 1) {
 
         if($dataCountry){
               $dataCountryDecoded = json_decode($dataCountry, true);
+              var_dump($dataCountryDecoded);
         }else{
               header("Location: http://localhost:8080/tp-php/form-country.php/?response=error1");
               exit();
@@ -37,11 +40,18 @@ if(isset($_POST["country"]) && strlen($_POST["country"]) >= 1) {
 </head>
 <body>
 
-<p>Nom officiel : <?php echo htmlspecialchars($dataCountryDecoded[0]["name"]["official"])?></p>
-<p>Capital : <?php echo htmlspecialchars($dataCountryDecoded[0]["capital"][0]) ?> </p>
-<p>Population : <?php echo htmlspecialchars($dataCountryDecoded[0]["population"]) ?> </p>
-<p>Region : <?php echo htmlspecialchars($dataCountryDecoded[0]["region"]) ?> </p>
-<p>Drapeau : <img src="<?php echo htmlspecialchars($dataCountryDecoded[0]["flags"]["png"]) ?>"></p>
+<?php foreach ($dataCountryDecoded as $item) : ?>
+
+<div style="border : solid 1px black">
+    <p>Nom officiel : <?php echo htmlspecialchars($item["name"]["official"])?></p>
+    <p>Capital : <?php echo htmlspecialchars($item["capital"][0]) ?> </p>
+    <p>Population : <?php echo htmlspecialchars($item["population"]) ?> </p>
+    <p>Region : <?php echo htmlspecialchars($item["region"]) ?> </p>
+    <p>Drapeau : <img src="<?php echo htmlspecialchars($item["flags"]["png"]) ?>"></p>
+</div>
+
+
+<?php endforeach; ?>
 
 <a href="form-country.php">accueil</a>
 

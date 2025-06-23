016adafaaf72c0f9e3ff63cb59a0603d31c8aaf0
alexandre-mateos <mateosalexandre95@gmail.com>
2025-06-23 12:02:14 +0200

    changment de nom commun par nom officiel dans l'affichage

--- tp-php/form-treatment.php
+++ tp-php/form-treatment.php
@@ -1,17 +1,31 @@
 <?php
-if(isset($_POST["country"]) && strlen($_POST["country"]) >= 1){
+if(isset($_POST["country"]) && strlen($_POST["country"]) >= 1) {
       $country = trim(htmlspecialchars($_POST["country"]));
       $countryRequest = "https://restcountries.com/v3.1/name/" . $country . "?fullText=true";
       $dataCountry = file_get_contents($countryRequest);
       $dataCountryDecoded = json_decode($dataCountry, true);
-//      var_dump($dataCountryDecoded);
-      if(isset($dataCountryDecoded[0]["name"]["common"]) && strlen($dataCountryDecoded[0]["name"]["common"]) >= 1){
-            echo $dataCountryDecoded[0]["name"]["common"];
-      }else{
-            echo "on a un problème";
-      }
-
-}else{
-      echo "Vous devez remplir le champs";
+      var_dump($dataCountryDecoded);
 }
 ?>
+<!doctype html>
+<html lang="en">
+<head>
+      <meta charset="UTF-8">
+      <meta name="viewport"
+            content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+      <meta http-equiv="X-UA-Compatible" content="ie=edge">
+      <title>Document</title>
+</head>
+<body>
+
+<?php if(isset($dataCountryDecoded[0]["name"]["official"]) && strlen($dataCountryDecoded[0]["name"]["official"]) >= 1) : ?>
+<p> Nom officiel : <?php echo $dataCountryDecoded[0]["name"]["official"] ?></p>
+<?php else : ?>
+<p>Ce pays est inconnu</p>
+<?php endif; ?>
+
+
+</body>
+</html>
+
+

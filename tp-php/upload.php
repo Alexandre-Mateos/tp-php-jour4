e6a41a7e4e0d98621d4ef2601c05fcbd811651e3
alexandre-mateos <mateosalexandre95@gmail.com>
2025-06-23 16:29:14 +0200

    remplacement de certaines valeures par un boolean

--- tp-php/upload.php
+++ tp-php/upload.php
@@ -1,27 +1,26 @@
 <?php
 var_dump($_FILES);
-$target_directory = "uploads/";
-$target_file = $target_directory . uniqid() . htmlspecialchars(basename($_FILES["fileToUpload"]["name"])) ;
+$target_file = "uploads/" . uniqid() . htmlspecialchars(basename($_FILES["fileToUpload"]["name"])) ;
 $uploadOk = true;
 $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 
 
  // vérifier le format des fichiers
 if ($imageFileType !== "jpeg" && $imageFileType !== "png"){
-      echo "Désolé. Seul les fichiers au format jpeg et png sont acceptés";
+      echo "Désolé. Seul les fichiers au format jpeg et png sont acceptés"."<br>";
       $uploadOk = false;
 }
 
-// On vérifie si $uploadOk est à 0, et on informe l'utilisateur
+// On vérifie si $uploadOk est à false, et on informe l'utilisateur
 if (!$uploadOk) {
-      echo "Désolé. Chargement échoué";
+      echo "Chargement échoué"."<br>";
 
 // Sinon on lance le chargement
 } else {
       if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
             echo "The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.";
       } else {
-            echo "Sorry, there was an error uploading your file.";
+            echo "Désolé, erreur de chargement";
       }
 }
 
